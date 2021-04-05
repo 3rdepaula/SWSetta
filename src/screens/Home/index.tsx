0b53a858935d46fb57797bedc157209baa9b2361
Ramon de Paula <ramonsobreiradepaula@gmail.com>
2021-04-05 20:44:18 -0300

    feat: adicionar condição na Home para pagamento

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,15 +2,26 @@ import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native'
 import PersonCardMain from '../../components/PersonCardMain'
 import ModalPayment from '../../components/ModalPayment'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 function Home (): JSX.Element {
   const [time, setTime] = useState<boolean>(false)
 
+  const verifyPayment = async (): Promise<void> => {
+    await AsyncStorage.getItem('@keyValuePayment').then((response) => {
+      if (response === 'Pagar') {
+        setTime(false)
+      } else {
+        setTimeout(function () {
+          setTime(true)
+        }, 5000)
+      }
+    })
+  }
+
   useEffect(() => {
     // Modal para efetuar pagamento
-    setTimeout(function () {
-      setTime(true)
-    }, 45000)
+    verifyPayment()
   }, [])
 
   return (
